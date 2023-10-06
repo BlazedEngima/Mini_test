6be7f5edb0c1555f9bb8450fd7a7f3dd57ab44d1
Blazed <samueltheofie@gmail.com>
2023-10-06 20:09:29 +0700

    Added comments

--- weather/weather.py
+++ weather/weather.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 
 api_key = config.api_key
 
-
+# Function to use the geocode API call from openweather map to get lat, lon of cities
 def getGeoCode(city):
     url = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={api_key}'
 
@@ -21,32 +21,40 @@ def getGeoCode(city):
 
     return lat, lon
 
+# Function to get transform a string with the current date to a datetime object
 def getDay(date):
     return datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
 
+# Helper function to print out dictionary value in a proper string format
 def print_dict(data):
     for key, value in data.items():
         date = key.strftime('%a, %d %b %Y')
         print(f'{date}: {value:.2f}\xb0C')
 
+# Driver code
 if __name__ == "__main__":
+    # Get proper city and units 
     city = 'Jakarta'
-    count = 5
     units = 'metric'
 
+    # Get lat, lon of the city
     lat, lon = getGeoCode(city)
 
+    # Query the openweathermap api to get the weather data forecast for the next 5 days in 3 hour intervals
     url = f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units={units}&appid={api_key}'
     response = requests.get(url)
 
+    # Error handling
     if response.status_code != 200:
         print('Error fetching weather data')
 
     data = response.json()
 
+    # Storage for temperature and day counts
     temp_avg = defaultdict(lambda: 0)
     counter = defaultdict(lambda: 0)
 
+    # Add temperature of the same days (excluding today) 
     for time in data['list']:
         day = getDay(time['dt_txt'])
 
@@ -59,8 +67,10 @@ if __name__ == "__main__":
         temp_avg[day.date()] += temp
         
     
+    # Get the temperature average
     for key in temp_avg.keys():
         temp_avg[key] /= counter[key]
 
+    # Print the result
     print('Weather Forecast:')
     print_dict(temp_avg)
